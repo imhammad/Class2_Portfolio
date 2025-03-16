d6b8788503d298f621296204a539a701a61bf348
Hammad Hassan <hammadhassan319@gmail.com>
2025-03-16 17:38:17 +0800

    First commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const submitBtn = document.getElementById("submitBtn");
+const formMessage = document.getElementById("formMessage");
+
+function formBtn(){
+        formMessage.innerHTML = "Duhh Fill out the form Correctly!!";
+}
